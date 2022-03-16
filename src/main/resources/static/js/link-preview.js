58d6852685b42075fdaecd6740d0f3315300f19e
hristijankocev <hristijankocev@ymail.com>
2022-03-16 18:45:07 +0100

    Fixed bug on edit page that caused link fields to be empty at form submitting, added link preview option for valid links

--- src/main/resources/static/js/link-preview.js
+++ src/main/resources/static/js/link-preview.js
@@ -2,10 +2,19 @@ $(document).ready(function ($) {
     /* ------------------------- */
     /* Contact Form Interactions */
     /* ------------------------- */
+    const loading = $('#loading');
+    const linkInfo = $('#link-info');
+    const infoFragment = $('.cd-popup');
+
     let clickedLink = $('.preview-link');
-    clickedLink.on('click', function () {
+    clickedLink.on('click', function (event) {
+        event.preventDefault();
+        infoFragment.show();
+        loading.show();
+        linkInfo.hide()
+
         let data = {
-            linkId: clickedLink.attr("linkId")
+            linkId: $(this).attr("linkId")
         }
 
         addData(data);
@@ -14,7 +23,7 @@ $(document).ready(function ($) {
     });
 
     // close popup when clicking x or off popup
-    $('.cd-popup').on('click', function (event) {
+    infoFragment.on('click', function (event) {
         if ($(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup')) {
             event.preventDefault();
             $(this).removeClass('is-visible');
@@ -23,28 +32,41 @@ $(document).ready(function ($) {
 
     // close popup when clicking the esc keyboard button
     $(document).keyup(function (event) {
-        if (event.which === '27') {
-            $('.cd-popup').removeClass('is-visible');
+        if (event.key === "Escape") {
+            infoFragment.removeClass('is-visible');
         }
     });
+
+    function addData(data) {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8080/webpage/preview",
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            crossDomain: false,
+            dataType: "json",
+
+            success: function (data) {
+                console.log(data);
+                $('#loading').hide();
+                $('#link-info').show();
+
+                $('#link-title').text(data['ogTitle']);
+                $('#link-description').text(data['ogDesc']);
+                $('#link-img').attr('src', data['ogImage']);
+                $('#link-url')
+                    .text(data['ogUrl'])
+                    .attr('href', data['ogUrl']);
+                // $('#link-url').;
+            },
+
+            error: function (jqXHR, status) {
+                $('#link-title').text("Failed to fetch data.");
+                loading.classList.remove('display');
+                console.log(status);
+                console.log(jqXHR);
+            }
+        });
+    }
 });
 
-function addData(data) {
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/webpage/preview",
-        data: data,
-        contentType: "application/json; charset=utf-8",
-        crossDomain: false,
-        dataType: "json",
-
-        success: function (data) {
-            console.log(data);
-        },
-
-        error: function (jqXHR, status) {
-            console.log(status);
-            console.log(jqXHR);
-        }
-    });
-}
